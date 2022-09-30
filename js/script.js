817a2380c363414de04faf3888b4cfa3a5fca495
Guilherme <guilhermesouzabenso@gmail.com>
2022-09-30 01:46:45 -0300

    new: novas linhas adicionadas

--- js/script.js
+++ js/script.js
@@ -6,7 +6,24 @@ const cityInput = document.querySelector("#input-cidade");
 const pesquisa = document.querySelector("#pesquisar");
 
 
+const cityElement = document.querySelector("#cidade");
+const tempElement = document.querySelector("#temperatura span");
+const descriptionlement = document.querySelector("#descricao");
+const weatherIconElement = document.querySelector("#weather-icon");
+const PaisElement = document.querySelector("#pais");
+const umidadeElement = document.querySelector("#umidade");
+const windElement = document.querySelector("#wind span");
 
+
+const getWeatherData = async(cidade)=>{
+    const apiWeathrURL = 'https://api.openweathermap.org/data/3.0/onecall?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_bt';
+    
+}
+
+
+const showWeatherData = (cidade)=>{
+
+}
 
 
 
