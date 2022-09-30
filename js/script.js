378f2fec4cbec73e51d5db6b49a3a630bca416df
Guilherme <guilhermesouzabenso@gmail.com>
2022-09-30 00:50:22 -0300

    new: novas linhas adicionadas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-//34ef722f9693ef4a86146c2ab474f4f5
+const apiKey = "34ef722f9693ef4a86146c2ab474f4f5";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+
+const cityInput = document.querySelector("#input-cidade");
+const pesquisa = document.querySelector("#pesquisar");
+
+
+
+
+
+
+pesquisa.addEventListener("click",()=>{})
